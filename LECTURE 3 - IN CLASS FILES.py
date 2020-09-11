f2dc2dfc5183212704467315d8f4b828cadd7622
DrGMC <47373280+DrGMC@users.noreply.github.com>
2020-09-10 21:21:57 -0400

    Add files via upload

---
+++ LECTURE 3 - IN CLASS FILES.py
@@ -0,0 +1,151 @@
+##a = 33
+##b = 200
+##if b > a:
+#### print("b is greater than a")
+##
+##
+##  X = 100 # CONSTANT
+
+##
+##if x % 2 == 0:
+##    print("x =", x, "is even")
+##else:
+####    print("x =", x, "is odd")
+####
+####print(" NEW EXAMPLE-->")              # Nohting
+####print()              # Nohting
+####
+####
+####a = 100
+####b = 500
+####if b > a:
+####  print("b is greater than a")
+####else:
+####  print("b is not greater than a")
+####
+####print(" NEW EXAMPLE-->")              # Nohting
+####print()              # Nohting
+##
+##
+##s1 = "This"
+##
+##print(s1[1:])                    # print last three characters 
+##print(len(s1))                               # get the string length
+##print("Length of string is " + str(len(s1))) # type casting int to str
+##print(s1.upper())                            # convert to upper case
+##print(s1.lower())                            # convert to lower case
+##
+##s2 = "This is a string"
+##words = s2.split(' ')             # split the string into words
+##print(words[0])
+##print(s2.replace('a','another'))  # replace "a" with "another"
+##print(s2.replace('is','at'))      # replace "is" with "at"
+##print(s2.find("a"))               # find the position of "a" in s2
+##print(s1 in s2)                   # check if s1 is a substring of s2
+##
+##print(s1 == 'This')               # equality comparison
+##print(s1 < 'That')                # inequality comparison
+##print(s2 + " too")                # string concatenation
+##print((s1 + " ")* 3)              # replicate the string 3 times
+##
+##print()              # Nohting
+##print(" NEW EXAMPLE-->")              # Nohting
+##print()              # Nohting
+##
+##
+##s2 = "This is a string"
+##
+##print(s1 == 'This')               # equality comparison
+##print(s1 != 'This')               # negativity comparison
+##print(s1 < 'That')                # inequality comparison
+##print(s1 <= 'That')               # inequality comparison
+##print(s2 + " too")                # string concatenation
+##print((s1 + " ")* 3)              # replicate the string 3 times
+##
+##print(" NEW EXAMPLE-->")              # Nohting
+##print()              # Nohting
+##
+##
+##X = 100 # CONSTANT
+##
+##if X > 0:
+##    print("x =", X, "is positive")
+##elif X < 0:
+##    print("x =", X, "is negative")
+##else:
+##    print("x =", X, "is neither positive nor negative")
+##
+##
+##print()              # Nohting
+##print()              # Nohting
+##
+##if False:
+##    print("It is cold.")
+##    if True:
+##        print("The heater is already on.")
+##    else:
+##        print("Turning the Heater on.")
+##else:
+##    print("It is warm enough.")
+
+##print(" NEW EXAMPLE-->")            # Nohting
+##print()              # Nohting
+##
+##
+##a = 1
+##b = 2
+##c = 2
+##print (a > b) #False
+##a < b #True
+##print (a >= b) #False
+##a <= b #True
+##
+####print(" NEW EXAMPLE-->")              # Nohting
+####print()                              # Nohting
+####
+####a = 100
+####b = 300
+####c = 200
+####if a > b and c > a:
+####  print("Both conditions are True")
+####
+####print(" NEW EXAMPLE-->")              # Nohting
+####print()              # Nohting
+####
+##
+a = 10
+b = 20
+c = 30
+
+ex1 = a < b
+ex2 = a < b and b < c
+ex3 = (a + b == c) and (b - 10 == a) and (c / 3 == a)
+
+print (ex1, ex2, ex3)
+
+##print(" NEW EXAMPLE-->")              # Nohting
+##print()              # Nohting
+##
+##a = 200
+##b = 33
+##c = 500
+##if a > b or a > c:
+##  print("At least one of the conditions is True")
+##
+##
+##
+##print(" NEW EXAMPLE-->")              # Nohting
+##print()              # Nohting
+##
+##
+##a = 10
+##b = 20
+##c = 30
+##
+##bool1 = True and (a > b)
+##bool2 = (not True) or (b != c)
+##bool3 = (True and (not False)) or (a > b)
+##bool4 = (a % b == 2) and ((not True) or False)
+##
+##print (bool1, bool2, bool3, bool4)
+##
